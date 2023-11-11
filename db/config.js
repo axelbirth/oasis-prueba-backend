e394801918dc89ae3de9262ca1a32da474ec9146
axelml99 <axel@birth.com.mx>
2023-11-10 22:02:25 -0600

    update config

--- db/config.js
+++ db/config.js
@@ -5,8 +5,8 @@ const{config} = require('./../config/config');
 // Protegemos nuestros datos mas importantes
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-
+//const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = config.dbLink;
 module.exports = {
   development: {
     url: URI,
